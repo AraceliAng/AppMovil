e7b95c3d3abf2b5a1b3e66eec345f09e19eb58db
Araceli Ang <olguinaraceli@gmail.com>
2019-01-22 16:54:44 -0600

    segunda modificacion de tickets

--- src/components/tickets/FormTickets.js
+++ src/components/tickets/FormTickets.js
@@ -1,30 +1,45 @@
 import React, { Component } from 'react';
-import { View} from 'react-native';
-import { Header, Left, Button, Body,Right, Container, Title} from 'native-base';
+import { View, KeyboardAvoidingView, Image, StatusBar} from 'react-native';
+import { Header, Text, Form, Item, Left, Input, Button, Body,Right, Container, Title} from 'native-base';
 import Icon from 'react-native-vector-icons/Entypo';
+import styles6 from './Styles';
 export default class FormTickets extends Component{
     render(){
         return(
-           <Container>
-                <Header
-                    style={{ backgroundColor: '#000000' }}
-                    androidStatusBarColor="black"
-                >
-                    <Left>
-                        <Button transparent onPress={()=>Actions.pop()}>
-                            <Icon name='menu' style={{color:'white'}} size={20} />
-                        </Button>
-                    </Left>
+           <Container >
+            <Header
+                        style={{ backgroundColor: '#000000' }}
+                        androidStatusBarColor="black"
+            >
+                <Left>
+                    <Button transparent onPress={()=>Actions.pop()}>
+                        <Icon name='menu' style={{color:'white'}} size={20} />
+                    </Button>
+                </Left>
                     <Body>
-                        <Title> Tickets</Title>
+                        <Title>Tickets o facturas</Title>
                         </Body>
                     <Right/>
-                </Header>
-            <View >
+            </Header>
             
-                
-            </View>
-        </Container>
+          
+                <Form style={styles6.containerF}>
+                    <Item regular style={styles6.inputs}>
+                        <Icon active name='edit' size={15} style={{marginLeft:12}} />
+                        <Input 
+                            name='desc'
+                            placeholder='Descripción'
+                            style={styles6.textoF}
+                            onChangeText={Value=>onChange('desc',Value)}
+                        />
+                    </Item>
+                    <Button full bordered dark  style={{borderRadius:25, borderColor:'#5F0003'}}>
+                        <Text>Tomar foto</Text>
+                    </Button>
+                </Form>
+           
+            <StatusBar backgroundColor="black" barStyle="light-content" />
+            </Container>
         );
     }
 }
