0db1d5cb4bd17cc1da8a17db0f9749d77b3ef8f7
Araceli Ang <olguinaraceli@gmail.com>
2019-01-22 16:13:27 -0600

    modificacion de tickets

--- src/components/tickets/FormTickets.js
+++ src/components/tickets/FormTickets.js
@@ -1,32 +1,48 @@
 import React, { Component } from 'react';
-import { View} from 'react-native';
-import { Header, Left, Button, Body,Right, Container, Title} from 'native-base';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { View, StatusBar} from 'react-native';
+import { Header, Left, Button, Body,Right, Container, Title, Input, Item, Form, Text} from 'native-base';
+import Icon from 'react-native-vector-icons/Entypo';
+import styles6 from './Styles';
 export default class FormTickets extends Component{
     render(){
         return(
            <Container>
                 <Header
-                    style={{ backgroundColor: '#000565' }}
+                    style={{ backgroundColor: '#000000' }}
                     androidStatusBarColor="black"
                 >
-                    <Left>
+                    <Left >
                         <Button transparent onPress={()=>Actions.pop()}>
-                            <Icon name='navicon' style={{color:'white'}} size={20} />
+                            <Icon name='menu' style={{marginRight: 30, fontSize: 20, color:'#DCDCDC'}} />
                         </Button>
                     </Left>
                     <Body>
-                        <Title> Tickets</Title>
-                        </Body>
-                    <Right/>
+                        <Title style={{color:'#DCDCDC', fontSize: 20}}>Tickets o facturas</Title>
+                    </Body>
+                    <Right>
+                        <Button transparent  onPress={this.logOut}>
+                            <Text style={{color:'#DCDCDC', fontSize: 17}}>Cerrar sesión</Text>
+                        </Button>
+                    </Right>
                 </Header>
-            <View >
-            
-                
-            </View>
+
+                <Form style={styles6.containerF}>
+                <Item regular style={styles6.inputs}>
+                    <Icon active name='user' size={15} style={{marginLeft:12}} />
+                    <Input 
+                        name='descripcion'
+                        placeholder='Escribre una descripción'
+                        keyboardType='descripcion'
+                        style={styles6.textoF}
+                        onChangeText={Value=>onChange('descripcion',Value)}
+                    />
+                </Item>
+                <Button full bordered dark onPress={} style={{borderRadius:25, borderColor:'#5F0003'}}>
+                    <Text>Subir imagen</Text>
+                </Button>
+            </Form>
+            <StatusBar backgroundColor="black" barStyle="light-content" />
         </Container>
         );
     }
 }
-
-//<Icon name='chevron-left' style={{color:'white'}} size={20} />
---
+++ src/components/tickets/Styles.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+
+const styles6= StyleSheet.create({
+    textoF:{
+        color:'#000000',
+        fontSize: 20,
+    },
+    textoL:{
+        padding:20,
+        color:'#000000',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    inputs:{
+      height:40,
+      backgroundColor:'rgba(255, 255, 255, 0.9)',
+      marginBottom:20,
+      borderRadius:10
+    },
+    containerF:{
+        padding:20,
+        alignItems:'center',
+        
+    },
+    
+    containerR:{
+        padding:10,
+    },
+    
+});
+
+
+export default styles6
