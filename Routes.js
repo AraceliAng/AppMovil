68cb5fb2b9939f27de2cf3dd846efd9cb290bd77
Araceli Ang <olguinaraceli@gmail.com>
2019-01-28 17:09:26 -0600

    otra cambio

--- Routes.js
+++ Routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Root} from 'native-base';
-import {Router, Scene} from 'react-native-router-flux';
+import { StyleSheet } from 'react-native';
+import {Router, Scene, Drawer} from 'react-native-router-flux';
 import MainPage from './src/components/main/MainPage';
 import ComponentLogin from './src/components/login/ComponentLogin';
 import FormLocation from './src/components/location/FormLocation';
@@ -9,22 +10,38 @@ import FormTickets from './src/components/tickets/FormTickets';
 import ProfileBanner from './src/components/profile/ProfileBanner';
 const Routes = () => {
     return (
-        <Root>
+            
+        <Root style={stylesR.container}>
             <Router>
-
-                <Scene key="root">
+                 
+                    <Scene key="root">
 {/*se declara una escena con un key que es igual al nombre de la vista 
     ese nombre nos servira para poder navegar con el metodo Actions.nombredevista()
 */ }
-                    <Scene key="login" header={null} component={ComponentLogin}  inital />
-                    <Scene key="main" header={null} component={MainPage}/>
-                    <Scene key="profile" header={null} component={ProfileBanner}  />
-                    <Scene key="check" header={null} component={FormLocation} />
-                    <Scene key="tickets" header={null} component={FormTickets}/>
-                    <Scene key="reports" header={null} component={FormReports}/>
+           
+                    <Scene key="login"      header={null} component={ComponentLogin} initial/>
+                    <Scene key="main"       header={null} component={MainPage}/>
+                    <Scene key="profile"    header={null} component={ProfileBanner}  />
+                    <Scene key="location"   header={null} component={FormLocation} />
+                    <Scene key="tickets"    header={null} component={FormTickets}/>
+                    <Scene key="reports"    header={null} component={FormReports}/> 
+
                 </Scene>
-            </Router>
+               
+            </Router> 
+            
         </Root>
+       
     );
 }
 export default Routes;
+
+const stylesR= StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      //alignItems: 'center',
+      backgroundColor: '#ffffff',
+    },
+
+});
