689a07caa7db31b4eca4e8c06a8e519ff0c02464
AraceliAng <olguinaraceli@gmail.com>
2019-03-01 15:52:11 -0600

    mas modificaciones de registro

--- src/components/admin/Registry.js
+++ src/components/admin/Registry.js
@@ -1,59 +1,79 @@
 import React, { Component } from 'react';
-import { View, Image, StatusBar,Platform} from 'react-native';
-import { Header, Text, Item, Left, Input, Button, Body,Right, Container, Title, Drawer, Card, CardItem, Content} from 'native-base';
+import { StatusBar,Platform} from 'react-native';
+import { Toast, Header, Text, Item, Left, Input, Button, Body,Right, Container, Title, Drawer, Card, CardItem, Content} from 'native-base';
 import Icon from 'react-native-vector-icons/Entypo';
 import styles6 from './Styles';
 import SideBar from '../main/SideBar';
 import firebase, {firebaseAuth} from '../../services/firebase/Firebase';
 import {Actions} from 'react-native-router-flux';
 
-export default class Registry extends Components{
+export default class Registry extends Component{
 	state={
-        correo:'',
-        name: '',
-	    noEmp:'',
-	    pass:'',
-	    passw:'',
-	    area:'',
-	    cargo:'',
+        area:'',
+        cargo:'',
+        contraseña:'',
+        emailE:'',
+        nombre: '',
+	    numEmpleado:'',
 	    loading: false,
-        nombrePro:'',
-        vehiculo:'',
-        proDias:'',
-        destino:''
+        error:'',	    
+        nomProyecto:'',
+        vehiculoAsig:'',
+        diasEstimados:'',
+        destino:'',
     };
-    constructor(props){
+    
+
+    constructor(props) {
         super(props);
         this.onLoginSuccess = this.onLoginSuccess.bind(this);
-        this.onLoginFailed= this.onLoginFailed.bind(this);
+        this.onLoginFailed = this.onLoginFailed.bind(this);
     }
+
+
     onButtonPress(){
-        const{name, noEmp, pass, passw, area, cargo,loading, finder,  nombrePro, vehiculo, proDias, destino}= this.state;
-        this.setState({error: '', loading:true});
-        if (pass == passw && pass != null && passw != null && correo != null && name != null && noEmp != null && area != null && cargo !=null && nombrePro != null && vehiculo != null && proDias != null && destino != null ) {
-            console.log(this.state.passw)
-            console.log(this.state.pass)
-            console.log(this.state.correo)
-            firebaseAuth.createUserWithEmailAndPassword(correo,password);
+        const{ area, cargo, contraseña, emailE, nombre,  numEmpleado }= this.state;
+        //   nomProyecto, vehiculoAsig, diasEstimados, destino
+        //this.setState({error: '', loading:true});
+        if ( area != null && cargo != null && contraseña != null  && emailE != null && nombre != null  && numEmpleado != null  /**  && nomProyecto != null && vehiculoAsig != null && diasEstimados != null && destino != null  */  ) {
+            console.log(this.state.contraseña)
+            console.log(this.state.emailE)
+            firebaseAuth.createUserWithEmailAndPassword(emailE, contraseña).then(this.onLoginSuccess).catch(this.onLoginFailed);
         }
         else {
             Toast.show({ 
-            text: 'Llene los campos correctamente',
+            text: 'Verifique que los datos sean correctos',
             position: 'bottom',
             buttonText: 'OK',
             type: 'danger'
             })
         }
     }
-    buttonRegistry(){
-        if (this.state.loading) {
-            return (
-            <Button full rounded style={styles6.boton} onPress={this.onButtonPress.bind(this)}  >
-            <Text style={{color:'#FFFFFF'}}>Registrar</Text>
-            </Button>
-            );
-        }
+    onLoginFailed() {
+        this.setState({error: 'Autenticación Fallida', loading:false});
+        Toast.show({ text: 'Verifique los campos',position: 'bottom', buttonText: 'OK', type: 'danger'})
     }
+
+    onLoginSuccess(userLog) {
+        var uid = userLog.uid;
+        console.log(userLog)
+      try{
+        firebase.database().ref('/empleado/' + uid +"/").set({
+            area: this.state.area,
+            cargo: this.state.cargo,
+            contraseña: this.state.contraseña,
+            emailE: this.state.emailE,
+            nombre: this.state.nombre,
+            numEmpleado: this.state.numEmpleado,
+       });
+        this.setState({emailE: '', contraseña: '', error: '', loading: false});
+        Toast.show({text: 'Bienvenido', position: 'bottom', duration: 3000, type: 'success'})
+      }catch(error){
+        console.log(error)
+      }
+  
+    }
+
     render(){
 
         closeDrawer = () => {
@@ -64,143 +84,160 @@ export default class Registry extends Components{
         this.drawer._root.open()
         };
 
-        let {user,logged}=this.state
+        // let {userLog,loggedIn}=this.state
         return(
 
-    <Container >
+            <Container >
 
-    <Drawer
-        ref={(ref) => { this.drawer = ref; }}
-        content={<SideBar navigator={this.navigator} logged={logged}/>}
-        onClose={this.closeDrawer} >
+                <Drawer
+                    ref={(ref) => { this.drawer = ref; }}
+                    content={<SideBar navigator={this.navigator} />}
+                    // loggedIn={loggedIn}
+                    onClose={this.closeDrawer} >
 
-        <Header style={{ backgroundColor: '#000000', paddingTop:22, height:80}}
-                androidStatusBarColor="black">
-            <Left>
-                <Button transparent onPress={openDrawer}>
-                    <Icon name='menu' style={{marginRight: 5, fontSize: 30, color:'#DCDCDC'}} />
-                </Button>
-            </Left>
+                    <Header style={{ backgroundColor: '#000000', paddingTop:22, height:80}}
+                            androidStatusBarColor="black">
+                        <Left>
+                            <Button transparent onPress={openDrawer}>
+                                <Icon name='menu' style={{marginRight: 5, fontSize: 30, color:'#DCDCDC'}} />
+                            </Button>
+                        </Left>
 
-            <Body>
-                <Title style={{color:'#DCDCDC'}}>
-                    {logged ? user.username : "Registro" }
-                </Title>
-            </Body>
+                        <Body>
+                            <Title style={{color:'#DCDCDC'}}>
+                                {/* {loggedIn ? userLog.username : "Registro" } */}
+                            </Title>
+                        </Body>
 
-        </Header>
+                    </Header>
 
-        <Content>
+                    <Content>
 
-        <Card style={styles6.containerF}>
-
-                <CardItem>
-                    <Left>
-                        <Text>OPERADORES</Text>
-                    </Left>
-                    <Right/>
-                </CardItem>
-
-                <CardItem>
-                <Body>
-                                        <Item regular style={styles6.inputs}>
-                                            {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
-                                                <Input name='nomb' placeholder='Nombre completo' style={styles6.textoF} onChange={this.handleChange} />
-                                        </Item>
-                                    </Body>     
-                                </CardItem>
-
-                                <CardItem>
-                                    <Body>
-                                        <Item regular style={styles6.inputs}>
-                                            {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
-                                                <Input name='num' placeholder='Número de empleado' style={styles6.textoF} onChange={this.handleChange} />
-                                        </Item>
-                                    </Body>     
-                                </CardItem>
-
-                                <CardItem>
-                                    <Body>
-                                        <Item regular style={styles6.inputs}>
-                                            {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
-                                                <Input name='area' placeholder='Área' style={styles6.textoF} onChange={this.handleChange} />
-                                        </Item>
-                                    </Body>     
-                                </CardItem>
-
-                                <CardItem>
-                                    <Body>
-                                        <Item regular style={styles6.inputs}>
-    {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
-    <Input name='pass' placeholder='Password' style={styles6.textoF} onChange={this.handleChange} />
-                                        </Item>
-                                    </Body> 
-                                </CardItem>
-
-                                <CardItem>
-    {this.buttonRegistry() }                                
-                                </CardItem>
-
-                            </Card>
-
-                            <Card style={styles6.containerF}>
-                                <CardItem>
-                                    <Left>
-                                        <Text>PROYECTOS</Text>
-                                    </Left>
-                                    <Right/>
-                                </CardItem>
-
-                                <CardItem>
-                                    <Body>
-                                        <Item regular style={styles6.inputs}>
-                                            {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
-                                                <Input name='nomProyecto' placeholder='Nombre del proyecto' style={styles6.textoF} onChange={this.handleChange} />
-                                        </Item>
-                                    </Body>     
-                                </CardItem>
-
-                                <CardItem>
-                                <Body>
-                                        <Item regular style={styles6.inputs}>
-                                            {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
-                                                <Input name='vehiculo' placeholder='Vehículo asignado' style={styles6.textoF} onChange={this.handleChange} />
-                                        </Item>
-                                    </Body>     
-                                </CardItem>
-
-                                <CardItem>
-                                    <Body>
-                                        <Item regular style={styles6.inputs}>
-                                            {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
-                                                <Input name='destino' placeholder='Destino' style={styles6.textoF} onChange={this.handleChange} />
-                                        </Item>
-                                    </Body>     
-                                </CardItem>
-
-                                <CardItem>
-                                    <Body>
-                                        <Item regular style={styles6.inputs}>
-                                            {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
-                                                <Input name='dias' placeholder='Número de días' style={styles6.textoF} onChange={this.handleChange} />
-                                        </Item>
-                                    </Body>  
-                                </CardItem>
+                        <Card style={styles6.containerF}>
 
                             <CardItem>
-    {this.buttonRegistry() }                                
-                                </CardItem>
+                                <Left>
+                                    <Text>OPERADORES</Text>
+                                </Left>
+                                <Right/>
+                            </CardItem>
 
-                                </Card>
+                            <CardItem>
+                                <Body>
+                                    <Item regular style={styles6.inputs}>
+                                        {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
+                                        <Input name='area' placeholder='Área' style={styles6.textoF} onChange={this.handleChange} />
+                                    </Item>
+                                </Body>     
+                            </CardItem>
 
-    </Content>
+                            <CardItem>
+                                <Body>
+                                    <Item regular style={styles6.inputs}>
+                                        {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
+                                        <Input name='cargo' placeholder='Cargo dentro de proyecto' style={styles6.textoF} onChange={this.handleChange} />
+                                    </Item>
+                                </Body>     
+                            </CardItem>
 
-    </Drawer>
+                            <CardItem>
+                                <Body>
+                                    <Item regular style={styles6.inputs}>
+                                        {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
+                                        <Input name='contraseña' placeholder='Password' style={styles6.textoF} onChange={this.handleChange} />
+                                    </Item>
+                                </Body> 
+                            </CardItem>
 
-    <StatusBar backgroundColor="#efeff4" barStyle={Platform.OS === 'android' ? "dark-content": "default" } />
+                            <CardItem>
+                                <Body>
+                                    <Item regular style={styles6.inputs}>
+                                        {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
+                                        <Input name='emailE' placeholder='Correo electrónico' style={styles6.textoF} onChange={this.handleChange} />
+                                    </Item>
+                                </Body>     
+                            </CardItem>                       
 
-    </Container>
-    )
+                            <CardItem>
+                                <Body>
+                                    <Item regular style={styles6.inputs}>
+                                        {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
+                                        <Input name='nombre' placeholder='Nombre completo' style={styles6.textoF} onChange={this.handleChange} />
+                                    </Item>
+                                </Body>     
+                            </CardItem>
+
+                            <CardItem>
+                                <Body>
+                                    <Item regular style={styles6.inputs}>
+                                        {/* <Icon active name='edit' size={15} style={{marginLeft:12}} /> */}
+                                        <Input name='numEmpleado' placeholder='Número de empleado' style={styles6.textoF} onChange={this.handleChange} />
+                                    </Item>
+                                </Body>     
+                            </CardItem>
+                            
+                            <CardItem>
+                                <Button bordered dark style={styles6.boton} onPress={this.onButtonPress.bind(this)}>
+                                    <Text>Registrar</Text>
+                                </Button>                           
+                            </CardItem>
+
+                        </Card>
+                        {/* <Card style={styles6.containerF}>
+                            <CardItem>
+                                <Left>
+                                    <Text>PROYECTOS</Text>
+                                </Left>
+                                <Right/>
+                            </CardItem>
+
+                            <CardItem>
+                                <Body>
+                                    <Item regular style={styles6.inputs}>
+                                        <Input name='nomProyecto' placeholder='Nombre del proyecto' style={styles6.textoF} onChange={this.handleChange} />
+                                    </Item>
+                                </Body>     
+                            </CardItem>
+
+                            <CardItem>
+                                <Body>
+                                    <Item regular style={styles6.inputs}>
+                                        <Input name='vehiculoAsig' placeholder='Vehículo asignado' style={styles6.textoF} onChange={this.handleChange} />
+                                    </Item>
+                                </Body>     
+                            </CardItem>
+
+                            <CardItem>
+                                <Body>
+                                    <Item regular style={styles6.inputs}>
+                                        <Input name='destino' placeholder='Destino' style={styles6.textoF} onChange={this.handleChange} />
+                                    </Item>
+                                </Body>     
+                            </CardItem>
+
+                            <CardItem>
+                                <Body>
+                                    <Item regular style={styles6.inputs}>
+                                        <Input name='diasEstimados' placeholder='Número de días' style={styles6.textoF} onChange={this.handleChange} />
+                                    </Item>
+                                </Body>  
+                            </CardItem>
+
+                            <CardItem>
+                                <Button full rounded style={styles6.boton} onPress={this.onButtonPress.bind(this)}  >
+                                    <Text style={{color:'#FFFFFF'}}>Registrar</Text>
+                                </Button>                            
+                            </CardItem>
+
+                        </Card> */}
+
+                    </Content>
+
+                </Drawer>
+
+                <StatusBar backgroundColor="#efeff4" barStyle={Platform.OS === 'android' ? "dark-content": "default" } />
+
+            </Container>
+            );
+        }
     }
-
-
--- src/services/firebase/Helpers.js
+++ src/services/firebase/Helpers.js
@@ -1,40 +1,40 @@
 import * as firebase from './Firebase';
 
 class Helpers {
-    static setUserName(userID, name){
-        let userNamePath = "/user/"+userID+"/Datos/name ";
-        return firebase.database().ref(userNamePath).set(name)
+    static setUserName(userID, nombre){
+        let userNamePath = "/empleado/"+userID+"/nombre ";
+        return firebase.database().ref(userNamePath).set(nombre)
     }
-    static setUserNemp(userID, nemp){
-        let userNamePath = "/user/"+userID+"/Datos/nemp ";
-        return firebase.database().ref(userNamePath).set(nemp)
+    static setUserNemp(userID, numEmpleado){
+        let userNamePath = "/empleado/"+userID+"/numEmpleado ";
+        return firebase.database().ref(userNamePath).set(numEmpleado)
     }
     static setUserArea(userID, area){
-        let userNamePath = "/user/"+userID+"/Datos/area ";
+        let userNamePath = "/area/"+userID+"/area ";
         return firebase.database().ref(userNamePath).set(area)
     }
     static setUserCargo(userID, cargo){
-        let userNamePath = "/user/"+userID+"/Datos/cargo";
+        let userNamePath = "/empleado/"+userID+"/cargo";
         return firebase.database().ref(userNamePath).set(cargo)
     }
-    static setUserPass(userID, pass){
-        let userNamePath = "/user/"+userID+"/Datos/pass";
-        return firebase.database().ref(userNamePath).set(pass)
+    static setUserPass(userID, contraseña){
+        let userNamePath = "/empleado/"+userID+"/contraseña";
+        return firebase.database().ref(userNamePath).set(contraseña)
     }
         static setUserNomProyecto(userID, nomproyecto){
-        let userNamePath = "/user/"+userID+"/Datos/nomproyecto";
+        let userNamePath = "/proyecto/"+userID+"/nomproyecto";
         return firebase.database().ref(userNamePath).set(nomproyecto)
     }
-    static setUserDiasEstim(userID, diasestim){
-        let userNamePath = "/user/"+userID+"/Datos/diasestim";
-        return firebase.database().ref(userNamePath).set(diasestim)
+    static setUserDiasEstim(userID, diasEstimados){
+        let userNamePath = "/proyecto/"+userID+"/diasEstimados";
+        return firebase.database().ref(userNamePath).set(diasEstimados)
     }
-    static setUserVehiculo(userID, vehiculo){
-        let userNamePath = "/user/"+userID+"/Datos/vehiculo";
-        return firebase.database().ref(userNamePath).set(vehiculo)
+    static setUserVehiculo(userID, vehiculoAsig){
+        let userNamePath = "/proyecto/"+userID+"/vehiculoAsig";
+        return firebase.database().ref(userNamePath).set(vehiculoAsig)
     }
     static setUserDestino(userID, destino){
-        let userNamePath = "/user/"+userID+"/Datos/destino ";
+        let userNamePath = "/proyecto/"+userID+"/destino ";
         return firebase.database().ref(userNamePath).set(destino)
     }
 }
