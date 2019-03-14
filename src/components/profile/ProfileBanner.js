37897a01bef4cee90ac8f1e338165beabc38ffd7
AraceliAng <olguinaraceli@gmail.com>
2019-03-13 19:48:05 -0600

    Perfil de los empleados u_u

--- src/components/profile/ProfileBanner.js
+++ src/components/profile/ProfileBanner.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import {View, Text, Image, StatusBar,Platform } from 'react-native';
-import { Content, Button, H1, Left, Body, Title, Header,Right, Card, ListItem, Drawer} from 'native-base';
+import { Content, Button, H1, snap ,Left, Body, Title, Header,Right, Card, ListItem, Drawer} from 'native-base';
 import Icon from 'react-native-vector-icons/Entypo';
 import SideBar from '../main/SideBar';
 import stylesP from './Styles';
 import imgFondo from '../../assets/mas.jpg';
 import HeaderImageScrollView, { TriggeringView } from 'react-native-image-header-scroll-view';
 import ProfileContent from './ProfileContent';
+import firebase from '../../services/firebase/Firebase';
 
 export default class ProfileBanner extends Component {
     state={
@@ -28,32 +29,15 @@ export default class ProfileBanner extends Component {
         }
     }
 
+    
+    
+    readUserData () { 
+        firebase.database().ref('empleado/').once('value',function(snapshot) { 
+            console.log(snapshot.val()) 
+        }); 
+    }
 
-    listenForItems = (itemsRef) => {
-        itemsRef.on('value', (snap) => {
-      
-          // get children as an array
-          var lista = [];
-          snap.forEach((child) => {
-            lista.push({
-              nombre: child.val().nombre,
-              numEmpleado: child.val().numEmpleado,
-              area: child.val().area,
-              cargo:child.val().cargo,
-              email: child.val().email,
-              password: child.val().password,
-              key: child.key
-      
-            });
-          });
-      
-      
-          this.setState({
-            lista: lista
-          });
-      
-        });
-      }
+    
     render() {
 
         closeDrawer = () => {
@@ -103,13 +87,70 @@ export default class ProfileBanner extends Component {
 
                     <Content>
                         <StatusBar backgroundColor="#efeff4" barStyle={Platform.OS === 'android' ? "dark-content": "default" }  />
-                            
-                            <View>
-                                <Card>
-                                    <ProfileContent lista={this.state.lista} />
-                                        {/* <ContenidoP lista={this.state.lista} /> */}
-                                </Card>   
-                            </View>
+                        <View>
+                <Card>
+                    <ListItem>
+                        <Icon name="clipboard" style={{marginRight: 30, fontSize: 20}} />
+                        <Body>
+                            <Text style={{fontWeight: 'bold'}}>Información</Text>
+                        </Body>
+                    </ListItem>
+                </Card>
+
+                <Card>
+                    <ListItem>
+                        <Body>
+                            {/* <Text style={{fontWeight: 'bold'}}>Nombre</Text> */}
+                            <Text note >{this.state.readUserData} </Text>
+                        </Body>
+                    </ListItem>
+                </Card>
+{/* 
+                <Card>
+                    <ListItem>
+                        <Body>
+                            <Text style={{fontWeight: 'bold'}}>Número de empleado</Text>
+                            <Text note> {this.state.lista}</Text>
+                        </Body>
+                    </ListItem>
+                </Card>
+        
+                <Card>
+                    <ListItem>
+                        <Body>
+                            <Text style={{fontWeight: 'bold'}}>Área</Text>
+                            <Text note> {this.state.area}</Text>
+                        </Body>
+                    </ListItem>
+                </Card>
+        
+                <Card>
+                    <ListItem>
+                        <Body>
+                            <Text style={{fontWeight: 'bold'}}>Cargo</Text>
+                            <Text note> {this.state.cargo}</Text>
+                        </Body>
+                    </ListItem>
+                </Card>
+
+                <Card>
+                    <ListItem>
+                        <Body>
+                        <Text style={{fontWeight: 'bold'}}>Correo Electrónico</Text>
+                        <Text note> {this.state.email}</Text>
+                        </Body>
+                    </ListItem>
+                </Card>
+
+                <Card>
+                    <ListItem>
+                        <Body>
+                            <Text style={{fontWeight: 'bold'}}>Contraseña</Text>
+                            <Text note> {this.state.password}</Text>
+                        </Body>
+                    </ListItem>
+                </Card> */}
+            </View>
                     </Content> 
 
                 </HeaderImageScrollView>
