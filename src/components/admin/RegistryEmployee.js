54be0c5615f1dc9b17128620cc14ac4cb559ade5
AraceliAng <olguinaraceli@gmail.com>
2019-04-02 17:45:48 -0600

    Registro de operadores

--- src/components/admin/RegistryEmployee.js
+++ src/components/admin/RegistryEmployee.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StatusBar,Platform } from 'react-native';
-import { Toast, Header, Text, Item, Left, Input, Button, Body, Right, Container, Title, Drawer, Card, CardItem, Content} from 'native-base';
+import { Picker,Toast, Header, Text, Item, Left, Input, Button, Body, Right, Container, Title, Drawer, Card, CardItem, Content} from 'native-base';
 import Icon from 'react-native-vector-icons/Entypo';
 import styles6 from './Styles';
 import SideBar from '../main/SideBar';
@@ -8,27 +8,44 @@ import firebase from '../../services/firebase/Firebase';
 
 export default class RegistryEmployee extends Component{
 	state={
-        area:'',
-        cargo:'',
         password:'',
         email:'',
         nombre: '',
-	    numEmpleado:'',
+        numEmpleado:'',
+        telefono:'',
 	    loading: false,
         error:'',	    
         userLog:{},
         loggedIn:false,
-        data:{}
+        data:{},
+        areas:[
+            {label:'Áreas ...'},
+            {label:'Sistemas'},
+            {label:'Diseño'},
+            {label:'Pesado'},
+            {label:'Mantenimiento'},
+        ],
+        cargos:[
+            {label:'Cargos ...'},
+            {label:'Jefe de área'},
+            {label:'Auxiliar de área'},
+            {label:'Personal de apoyo'},
+            {label:'Prácticante'},
+            {label:'Otro'},
+        ],
+        selectedCargo:'',
+        selectedArea:''
     };
     
 
     onButtonPress(){
         const{ password, email, data }= this.state;
         
-        if(Object.keys(data).length >= 6){
+        if(Object.keys(data).length >= 7){
             console.log(this.state.password)
             console.log(this.state.email)
             firebase.auth().createUserWithEmailAndPassword(email, password).then(r=>this.onSignupSuccess(r)).catch(e=>this.onSignupFailed(e));
+            
         }
         else {
             Toast.show({ 
@@ -49,27 +66,29 @@ export default class RegistryEmployee extends Component{
         var uid = userLog.user.uid;
         let{data}=this.state
         console.log('Usuario: ',userLog)
-      try{
-        firebase.database().ref('empleado/' + uid +'/').set({
-            area: data.area,
-            cargo: data.cargo,
-            password: data.password,
-            email: data.email,
-            nombre: data.nombre,
-            numEmpleado: data.numEmpleado
-       });
+        try{
+            firebase.database().ref('empleado/' + uid +'/').set({
+                area: data.area,
+                cargo: data.cargo,
+                password: data.password,
+                email: data.email,
+                nombre: data.nombre,
+                numEmpleado: data.numEmpleado,
+                telefono: data.telefono
+                
+        });
+        this.setState({data:{},uid:'',password:'',email:'',selectedArea:'',selectedCargo:'',telefono:'', error:'', loading:false})
+            // this.setState({email: '', password: '', error: '', loading: false});
+            Toast.show({text: 'Se ha agregado con éxito', position: 'bottom', type: 'success'})
+        
+        }catch(error){
+            console.log(error)
+        }
     
-        this.setState({email: '', password: '', error: '', loading: false});
-        Toast.show({text: 'Se ha agregado con éxito', position: 'bottom', type: 'success'})
-    
-      }catch(error){
-        console.log(error)
-      }
-  
     }
     handleChange=(field,value)=>{
         console.log('Antes',field,value)
-        let {data,email,password} = this.state
+        let {data,email,password,selectedCargo,selectedArea} = this.state
         if(field === 'email'){
             email = value
             this.setState({email})
@@ -77,6 +96,17 @@ export default class RegistryEmployee extends Component{
         if(field === 'password'){
             password = value
             this.setState({password})
+        }   
+        if(field === 'cargo'){
+            selectedCargo=value
+            this.setState({selectedCargo})
+        }if(field === 'area'){
+            selectedArea = value
+            this.setState({selectedArea})
+        }
+        if(field === 'telefono'){
+            telefono = value
+            this.setState({telefono})
         }
         data[field]=value
         this.setState({data})
@@ -93,7 +123,7 @@ export default class RegistryEmployee extends Component{
         openDrawer = () => {
         this.drawer._root.open()
         };
-        let {userLog,loggedIn}=this.state
+        let {userLog,loggedIn,cargos,areas}=this.state
         return(
 
             <Container >
@@ -142,24 +172,41 @@ export default class RegistryEmployee extends Component{
                             <CardItem>
                                 <Body>
                                     <Item regular style={styles6.inputs}>
-                                        <Input name='numEmpleado' placeholder='Número de empleado' style={styles6.textoF} onChangeText={value=>this.handleChange('numEmpleado',value)} />
+                                        <Input name='numEmpleado' keyboardType='number-pad' placeholder='Número de empleado' style={styles6.textoF} onChangeText={value=>this.handleChange('numEmpleado',value)} />
                                     </Item>
                                 </Body>     
                             </CardItem>
 
                             <CardItem>
                                 <Body>
-                                    <Item regular style={styles6.inputs}>
-                                        <Input name='area' placeholder='Área' style={styles6.textoF} onChangeText={value=>this.handleChange('area',value)} />
-                                    </Item>
+                                <Picker
+                                        note
+                                        mode="dropdown"
+                                        style={{width:'100%'}}
+                                        selectedValue={this.state.selectedArea}
+                                        onValueChange={value=>this.handleChange('area',value)}
+                                        >
+                                        {areas.map((area,i)=> <Picker.Item label={area.label} value={area.label} key={i}/> )}
+
+                                    </Picker>
                                 </Body>     
                             </CardItem>
 
                             <CardItem>
                                 <Body>
-                                    <Item regular style={styles6.inputs}>
+                                    <Picker
+                                        note
+                                        mode="dropdown"
+                                        style={{width:'100%'}}
+                                        selectedValue={this.state.selectedCargo}
+                                        onValueChange={value=>this.handleChange('cargo',value)}
+                                        >
+                                        {cargos.map((cargo,i)=> <Picker.Item label={cargo.label} value={cargo.label} key={i}/> )}
+
+                                    </Picker>
+                                    {/* <Item regular style={styles6.inputs}>
                                         <Input name='cargo' placeholder='Cargo dentro del proyecto' style={styles6.textoF} onChangeText={value=>this.handleChange('cargo',value)} />
-                                    </Item>
+                                    </Item> */}
                                 </Body>     
                             </CardItem>
 
@@ -180,6 +227,14 @@ export default class RegistryEmployee extends Component{
                             </CardItem>      
                             
                             <CardItem>
+                                <Body>
+                                    <Item regular style={styles6.inputs}>
+                                        <Input name='telefono' keyboardType='phone-pad' placeholder='Número de teléfono' style={styles6.textoF} onChangeText={value=>this.handleChange('telefono',value)} />
+                                    </Item>
+                                </Body> 
+                            </CardItem>      
+
+                            <CardItem>
                                 <Button bordered dark style={styles6.boton} onPress={this.onButtonPress.bind(this)}>
                                     <Text>Registrar</Text>
                                 </Button>                           
@@ -191,7 +246,7 @@ export default class RegistryEmployee extends Component{
 
                 </Drawer>
 
-                <StatusBar backgroundColor="#efeff4" barStyle={Platform.OS === 'android' ? "dark-content": "default" } />
+                <StatusBar backgroundColor="#DEDEDE" barStyle={Platform.OS === 'android' ? "dark-content": "default" } />
 
             </Container>
             );
