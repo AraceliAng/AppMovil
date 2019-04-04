e29d53662baf50af669bf5975bf5af7ceb26b30f
AraceliAng <olguinaraceli@gmail.com>
2019-04-04 15:31:57 -0600

    Registro de empleados

--- src/components/admin/RegistryEmployee.js
+++ src/components/admin/RegistryEmployee.js
@@ -246,7 +246,7 @@ export default class RegistryEmployee extends Component{
 
                 </Drawer>
 
-                <StatusBar backgroundColor="#DEDEDE" barStyle={Platform.OS === 'android' ? "dark-content": "default" } />
+                <StatusBar backgroundColor="#000000" barStyle={Platform.OS === 'android' ? "white-content": "default" }  />  
 
             </Container>
             );
