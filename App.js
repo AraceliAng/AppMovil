e09bb9e6a924f841d5bbcd45d3843f8670bd071a
Araceli Ang <olguinaraceli@gmail.com>
2019-04-11 11:40:52 -0500

    es app.js

--- App.js
+++ App.js
@@ -10,8 +10,8 @@ import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Routes from './Routes';
 //-----
-console.ignoredYellowBox = ['Setting a timer'];
-console.disableYellowBox = true;
+//console.ignoredYellowBox = ['Setting a timer'];
+//console.disableYellowBox = true;
 //-----
 export default class App extends Component{
   render() {
