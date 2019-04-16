657271bb6d63082301d73533062bc101e3c63de4
Araceli Ang <olguinaraceli@gmail.com>
2019-04-16 13:07:55 -0500

    modal empleado

--- src/components/admin/ModalEmpleado.js
+++ src/components/admin/ModalEmpleado.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react'
 import {Modal, TouchableHighlight, View, TouchableOpacity} from 'react-native';
-import { Input, Text,Item, Content, CheckBox, ListItem, Left, Button, Icon, Body, Right, Thumbnail, Header, Title } from 'native-base';
+import { Input, Text,Item, Content, CheckBox, ListItem, Left, Button, Body, Right, Thumbnail, Header, Title, Card, CardItem } from 'native-base';
 import imagen from '../../assets/user.png'
 import styles6 from './Styles';
 import firebase from '../../services/firebase/Firebase'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class ModalEmpleados extends Component{
     state={
         empleados:[],
         search:'',
-        selectedEmp:[]
+        selectedEmp:[],
     }
     //trae empleados
     getModal = () => {
@@ -19,6 +20,7 @@ export default class ModalEmpleados extends Component{
                     console.log('arreglo',Object.values(snapshot.val()))
                     let empleados = Object.values(snapshot.val())
                     this.setState({empleados})
+                    
                 });         
                
         } catch (error) {   
@@ -64,75 +66,87 @@ export default class ModalEmpleados extends Component{
     render(){
         let{close,open}=this.props
         //cosas del state
-        let{empleados, search,selectedEmp}=this.state
+        let{empleados, search,selectedEmp,checked}=this.state
         //funciones
         let{setEmp,borrarEmp,saveEmp}=this
         return(
 
-        <Modal
-          animationType="slide"
-          transparent={false}
-          visible={open}
-          onRequestClose={() => {
-            close()
-            this.setState({search:''})
-          }}>
-            <Header style={{ backgroundColor: '#000000'}}
+            <Modal
+            animationType="slide"
+            transparent={false}
+            visible={open}
+            onRequestClose={() => {
+              close()
+              this.setState({search:''})
+            }}>
+          <Header style={{ backgroundColor: '#000000', paddingTop:22, height:80}}
                             androidStatusBarColor="black">
-                <Left>
-                    <Button transparent >
-                        <Text>Cancelar</Text>
-                    </Button>
-                </Left>
+                        <Left>
+                            <Button transparent onPress={close}>
+                                {/* <Icon name='chevron-left' style={{marginRight: 5, fontSize: 30, color:'#DCDCDC'}} /> */}
+                                <Text>
+                                    <Icon name='chevron-left' style={{marginRight: 5, fontSize: 20, color:'#DCDCDC'}} />
+                                    Cancelar</Text>
+                            </Button>
+                        </Left>
 
-                <Body>
-                    <Title style={{color:'#DCDCDC'}}>
-                                 Empleados
-                    </Title>
-                </Body>
-                <Right>
-                <Button transparent onPress={saveEmp} >
-                    <Text>Aceptar</Text>
-                </Button>
-                </Right>
-            </Header>
-            <Item regular >
-                <Input 
-                    name='search' 
-                    placeholder='Buscar' 
-                    style={styles6.inputs}
-                    onChangeText={value=>this.handleChange('search',value)} 
-                    value={search}
-                />
-            </Item>
-            <View style={{flexDirection:'row'}}>
-                {selectedEmp.map((data,i)=>
-                <View key={i} style={{position:'relative',justifyContent:'center',alignItems:'center',width:70}}>
-                    <TouchableOpacity onPress={()=>borrarEmp(i)} style={{position:'absolute',top:-5,right:10,justifyContent:'center',alignItems:'center',zIndex:999, backgroundColor:'gray', width:20,height:20, borderColor:'#fff', borderWidth:1, borderRadius:50}}>
-                        {/* <Icon name='close' size={2} style={{color:'#fff',}}/> */}
-                        <Text style={{color:'#fff',fontSize:10}}>X</Text>
-                    </TouchableOpacity>
-                    <Thumbnail small source={imagen}/>
-                    <Text style={{fontSize:12}}>{data.numEmpleado}</Text>
-                </View>)}
-               
-            </View>
-            <Content>
-            {empleados.filter(emp=> emp.nombre.toLowerCase().includes(search.toLowerCase()) || emp.numEmpleado.toLowerCase().includes(search.toLowerCase()) ).map((empleado, i)=>
-            <ListItem onPress={()=>setEmp(empleado)} key={i} disabled={selectedEmp.includes(empleado)?true:false} >
-                <Left style={{flexDirection:'column',justifyContent:'flex-start'}}>
-                <Text>{empleado.nombre}</Text>
-                <Text note>{empleado.numEmpleado}</Text>
-                </Left>
-                <Right>
-                <CheckBox checked={selectedEmp.includes(empleado)?true:false} />
-                </Right>
-            </ListItem>)}
-            </Content>
-         
+                        <Body style={{ flex:1, alignItems:"center",justifyContent:'center'}}>
+                            <Text style={styles6.title}> 
+                                Selección de operadores 
+                            </Text>
+                        </Body>
+
+                        <Right>
+                            <Button transparent onPress={saveEmp} >
+                                <Text>Aceptar</Text>
+                            </Button>
+                        </Right>
+                </Header>
+                
+           
+           
+                <Card transparent style={styles6.containerR}>
+                    <CardItem>
+                        <Body>
+                            <Item regular style={styles6.inputs}>
+                                <Icon active name='search' size={15} style={{marginLeft:12}}/>
+                                <Input 
+                                    name='search' 
+                                    placeholder='Buscar' 
+                                    style={styles6.textoF} 
+                                    onChangeText={value=>this.handleChange('search',value)} 
+                                    value={search}
+                                />
+                            </Item>
+                        </Body>
+                    </CardItem>
+                </Card>
+                
+                            <View style={{flexDirection:'row'}}>
+                                {selectedEmp.map((data,i)=>
+                                <View key={i} style={{position:'relative',justifyContent:'center',alignItems:'center',width:70}}>
+                                    <TouchableOpacity onPress={()=>borrarEmp(i)} style={{position:'absolute',top:-5,right:10,justifyContent:'center',alignItems:'center',zIndex:999, backgroundColor:'gray', width:20,height:20, borderColor:'#fff', borderWidth:1, borderRadius:50}}>
+                                        <Text style={{color:'#fff',fontSize:10}}>X</Text>
+                                    </TouchableOpacity>
+                                    <Thumbnail small source={imagen}/>
+                                    <Text style={{fontSize:12}}>{data.numEmpleado}</Text>
+                                </View>)}
+                            </View>
+                
+                    <Content>
+                        {empleados.filter(emp=> emp.nombre.toLowerCase().includes(search.toLowerCase()) || emp.numEmpleado.toLowerCase().includes(search.toLowerCase()) ).map((empleado, i)=>
+                        <ListItem onPress={()=>setEmp(empleado)} key={i} disabled={selectedEmp.includes(empleado)?true:false} >
+                            <Left style={{flexDirection:'column',justifyContent:'flex-start', alignItems: 'stretch'}}>
+                                <Text>{empleado.nombre}</Text>
+                                <Text note>{empleado.numEmpleado}</Text>
+                            </Left>
+                            <Right>
+                                <CheckBox style={{borderColor:"#DAD8D8", }}
+                                            checked={selectedEmp.includes(empleado)? true :false} />
+                            </Right>
+                        </ListItem>)}
+                    </Content>
         </Modal>
-
-       
         )
     }
 }
