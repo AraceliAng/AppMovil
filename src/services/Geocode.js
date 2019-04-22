8147c28db65011b9b08a8a0c6c1672b7ca6b2f05
AraceliAng <olguinaraceli@gmail.com>
2019-04-21 23:24:35 -0500

    cambio en... geocode

--- src/services/Geocode.js
+++ src/services/Geocode.js
@@ -3,4 +3,7 @@ import axios from 'axios'
 
 export const getNamePosition = (region) => {
     return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${region.latitude},${region.longitude}&key=AIzaSyDzWxLHYJLLYIbuUtC6TgK7dbOeWJY7fAs`)
+   
+    //mucino
+    // return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${region.latitude},${region.longitude}&key=AIzaSyAXtlzhKjc93_oyhIGzwpIKmpDTFnzGhlc`)
 }
